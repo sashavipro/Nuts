c10c2204b5f56a7cf426911b875974bc21d4de62
sasha <sashavipro@gmail.com>
2026-02-28 20:28:34 +0300

    #16 base static/media_root

--- backend/mysite/settings/base.py
+++ backend/mysite/settings/base.py
@@ -200,10 +200,10 @@ STATICFILES_DIRS = [
 
 DJANGO_VITE_STATIC_URL_PREFIX = ""
 
-STATIC_ROOT = PROJECT_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = PROJECT_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 # Default storage settings
